45c70e2e0eb6378d8729b6c9de11c3374dd9ae0a
unknown <vikas.arisela@gmail.com>
2025-09-24 11:56:42 +0400

    variables practing

--- conditions.sh
+++ conditions.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-number=$1
-
-if [ $(($number % 2)) -eq 0]; then
+echo "give a number"
+read number
+if [ $(($number % 2)) -eq 0 ]; then
 echo "given number $number is even"
 else 
 echo "given number $number is odd"

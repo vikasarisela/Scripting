11eceb629b45113748b01b29cfcf7b8d23f87cbb
unknown <vikas.arisela@gmail.com>
2025-09-24 11:51:03 +0400

    variables practing

--- conditions.sh
+++ conditions.sh
@@ -3,7 +3,7 @@
 number=$1
 
 if [$(($number % 2) -eq 0)]; then
-echo "even number $number is even"
+echo "given number $number is even"
 else 
-echo "even number $number is odd"
+echo "given number $number is odd"
 fi

ca87e5bbe2fc6164fd3cb28e2ff6b88a52ca6d46
unknown <vikas.arisela@gmail.com>
2025-09-25 10:08:42 +0400

    variables-1.sh

--- conditions.sh
+++ conditions.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo "give a number"
-read number
-if [ $(($number % 2)) -eq 0 ]; then
-echo "given number $number is even"
-else 
-echo "given number $number is odd"
-fi
+# echo "give a number"
+# read number
+# if [ $(($number % 2)) -eq 0 ]; then
+# echo "given number $number is even"
+# else 
+# echo "given number $number is odd"
+# fi
 
 
 number=$1

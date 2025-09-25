298677b4e4437e0634616c1d344ebf14cb08496a
unknown <vikas.arisela@gmail.com>
2025-09-25 10:55:36 +0400

    variables-1.sh

--- conditions.sh
+++ conditions.sh
@@ -10,12 +10,21 @@
 
 
 
-NUMBER=$1
+# NUMBER=$1
 
-if [ $NUMBER -lt 10 ]; then
-    echo "Given number $NUMBER is less than 10"
-elif [ $NUMBER -eq 10 ]; then
-    echo "Given number $NUMBER is equal to 10"
+# if [ $NUMBER -lt 10 ]; then
+#     echo "Given number $NUMBER is less than 10"
+# elif [ $NUMBER -eq 10 ]; then
+#     echo "Given number $NUMBER is equal to 10"
+# else
+#     echo "Given number $NUMBER is greater than to 10"
+# fi
+
+
+number=$1
+
+if [$(($number % 2)) -eq 0]; then
+echo "the given number $number is even"
 else
-    echo "Given number $NUMBER is greater than to 10"
+echo "the given number $number is odd"
 fi

62bff63a99c953726563ba3fd39e6c8fee980772
unknown <vikas.arisela@gmail.com>
2025-09-25 10:13:32 +0400

    variables-1.sh

--- conditions.sh
+++ conditions.sh
@@ -12,7 +12,7 @@
 number=$1
 
 if [$number -lt 10]; then
-echo "$number is  less than 10"
+   echo "$number is  less than 10"
 else
-echo "$number is greater than 10"
+    echo "$number is greater than 10"
 fi

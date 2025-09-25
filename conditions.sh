763327d38aecbb72a40f2470e634fdb99549d059
unknown <vikas.arisela@gmail.com>
2025-09-25 10:07:13 +0400

    variables-1.sh

--- conditions.sh
+++ conditions.sh
@@ -7,3 +7,11 @@ echo "given number $number is even"
 else 
 echo "given number $number is odd"
 fi
+
+
+number=$1
+
+if [$((number -l 10))]; then
+echo "$number is  less than 10"
+else
+echo "$number is greater than 10"

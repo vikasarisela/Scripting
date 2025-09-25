44cffa280fad807267e4f606ffd3665b3a503150
unknown <vikas.arisela@gmail.com>
2025-09-25 11:27:12 +0400

    variables-1.sh

--- installation.sh
+++ installation.sh
@@ -7,9 +7,17 @@ echo "required root privliges"
 exit 1
 fi
 
-dnf install sql -y
-if [ $? -ne 0 ];then
-echo "installation failed"
+validate (){
+    if [ $1 -ne 0 ];then
+echo "$2 installation failed"
 else
-echo "installed successfully"
-fi   
+echo " $2 installed successfully"
+fi  
+}
+
+dnf install mysq -y
+validate $? "mysql"
+
+
+dnf install nginx -y
+validate $? "nginx"
